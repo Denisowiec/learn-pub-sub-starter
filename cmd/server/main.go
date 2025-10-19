3a0f9ae372e4ba74f9daab3c6926803d98ed11d6
Denisowiec <ddspeed@gmail.com>
2025-10-19 14:08:23 +0200

    Initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/Denisowiec/learn-pub-sub-starter/internal/pubsub"
+	"github.com/Denisowiec/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	log.Println("Starting Peril server...")
+	connAdr := "amqp://guest:guest@localhost:5672/"
+
+	rabbitConn, err := amqp.Dial(connAdr)
+	if err != nil {
+		log.Fatal("Error connecting to the RabbitMQ server: ", err)
+	}
+
+	defer rabbitConn.Close()
+	log.Println("Connection to RabbitMQ was successful.")
+
+	// Creating a channel for pause/unpause messages
+	pauseChan, err := rabbitConn.Channel()
+	if err != nil {
+		log.Fatal("Error creating a AMQP channel: ", err)
+	}
+
+	err = pubsub.PublishJSON(pauseChan, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Println("Failed to publish on the pauseChan: ", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	log.Println("Recieved interrupt signal. Exiting...")
 }
